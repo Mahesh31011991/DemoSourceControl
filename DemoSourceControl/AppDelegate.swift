e8b0a337249e2416553f7daa1d146d783ce0b49c
Mahesh Behere <maheshbehere14@gmail.com>
2022-03-31 19:26:49 +0530

    adds extra return to the app delegate

--- DemoSourceControl/AppDelegate.swift
+++ DemoSourceControl/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Demo Comment
+        return true
         return true
     }
 
